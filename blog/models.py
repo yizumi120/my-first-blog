bbff490437d988b6d7cbaa3582a46b7ddcdce858
izumi <yizumi120@gmail.com>
2019-09-25 16:12:38 +0900

    Cahnged the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -6,17 +6,15 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_data = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
-
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 
 # Create your models here.
